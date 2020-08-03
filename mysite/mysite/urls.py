b122fbb1321e5aeee2bf786c8650c2794b113f10
Will Chan <willwcchan@gmail.com>
2020-08-02 19:16:11 -0700

    Created my index view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    url(r'^optionchain/', include('optionchain.urls')),
     url(r'^admin/', admin.site.urls),
 ]
