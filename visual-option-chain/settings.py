e4fbd936c19dc75fdf00e3e15bcf6ea8d49772ef
Will Chan <willwcchan@gmail.com>
2020-09-10 22:28:30 -0700

    Needed to start redis-server

--- visual-option-chain/settings.py
+++ visual-option-chain/settings.py
@@ -112,7 +112,7 @@ else:
             'NAME': 'postgres',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
-            'HOST': 'db', # set in docker-compose.yml
+            'HOST': 'localhost', # set in docker-compose.yml
             'PORT': 5432,  # default postgres port,
             # https://docs.djangoproject.com/en/3.0/ref/settings/#test
             'TEST': {
