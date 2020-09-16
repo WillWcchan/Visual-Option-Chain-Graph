4405f013b1535f31fe21907b11e9e35fcad8cab8
Will Chan <willwcchan@gmail.com>
2020-09-15 17:04:41 -0700

    Added any host for now

--- visual-option-chain/settings.py
+++ visual-option-chain/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,"static"),
